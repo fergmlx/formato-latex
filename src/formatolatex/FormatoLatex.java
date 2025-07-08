59264e0c6bdb5b38524c015e05d5310b1e0ca53c
fer <115853012+fergmlx@users.noreply.github.com>
2025-07-08 16:02:38 -0600

    Agrega métodos estáticos - Se carga la librería JLatexMath - Se agregan métodos para el renderizado de imagen y asignación de formulas latex

---
+++ src/formatolatex/FormatoLatex.java
@@ -0,0 +1,65 @@
+package formatolatex;
+
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+import org.scilab.forge.jlatexmath.TeXIcon;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class FormatoLatex {
+    
+    public FormatoLatex() {
+    }
+    
+    public static boolean renderizarFormula(String formulaLatex, float textSize, String nombreArchivo) {
+        try {
+            TeXFormula formula = new TeXFormula(formulaLatex);
+            TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, textSize);
+            BufferedImage image = new BufferedImage(icon.getIconWidth(), 
+                                                    icon.getIconHeight(), 
+                                                    BufferedImage.TYPE_INT_ARGB);
+            
+            Graphics2D g2 = image.createGraphics();
+            g2.setColor(Color.WHITE);
+            g2.fillRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
+            g2.setColor(Color.BLACK);
+            icon.paintIcon(null, g2, 0, 0);
+            g2.dispose();
+            
+            // Guardar imagen
+            ImageIO.write(image, "PNG", new File(nombreArchivo + ".png"));
+            
+            System.out.println("Formula renderizada: " + nombreArchivo + ".png");
+            return true;
+            
+        } catch (Exception e) {
+            System.err.println("Error al renderizar formula: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public static boolean establecerMathTex(JLabel label, String formulaLatex, float tamaño) {
+        try {
+            TeXFormula formula = new TeXFormula(formulaLatex);
+            TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, tamaño);
+            
+            label.setIcon(icon);
+            label.setText("");
+            
+            return true;
+            
+        } catch (Exception e) {
+            System.err.println("Error al establecer fórmula en JLabel: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public static JLabel crearMathTex(String formulaLatex, float tamaño) {
+        JLabel label = new JLabel();
+        establecerMathTex(label, formulaLatex, tamaño);
+        return label;
+    }
+}
